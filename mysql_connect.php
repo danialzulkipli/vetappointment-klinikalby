331ac705ddc58607ea6ad5bdca16cb4be3a4aaac
Danial Zulkipli <41338620+danialzulkipli@users.noreply.github.com>
2022-03-24 10:10:31 +0800

    up

--- mysql_connect.php
+++ mysql_connect.php
@@ -4,7 +4,7 @@
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_DATABASE', 'vet');
+    define('DB_DATABASE', 'vetappointment_db');
     $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
     session_start();
@@ -15,5 +15,21 @@
     $mypassword = mysqli_real_escape_string($db, $_POST['password']);
 
     $sql = "SELECT * FROM login WHERE username = '$myusername' and password = '$mypassword'";
+
+    //check connection
+    if ($mysqli->connect_error) {
+        echo 'Error: '.$mysqli->connect_errno;
+        echo '<br>';
+        echo 'Error: '.$mysqli->connect_error;
+        exit();
+      }
+    
+      echo 'Success: A proper connection to MySQL was made.';
+      echo '<br>';
+      echo 'Host information: '.$mysqli->host_info;
+      echo '<br>';
+      echo 'Protocol version: '.$mysqli->protocol_version;
+    
+      $mysqli->close();
 
 ?>
